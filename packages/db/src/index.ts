dde95398f7df2120d289818dd65c8580ad23d01a
Khaled Zeitar <khalidzeiter9@gmail.com>
2025-12-15 18:12:00 +0100

    feat: set up database package with Prisma

---
+++ packages/db/src/index.ts
@@ -0,0 +1,11 @@
+import { env } from "@idea-sieve/env/server";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "../prisma/generated/client";
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma as db };
+
+export default prisma;
